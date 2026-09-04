a1dd1e7f806d2df05f1fcd9be4c00aa565bacc0d
Adityaraj Salunke <salunkeaditya0095@gmail.com>
2026-09-04 22:09:29 +0530

    Add files via upload

---
+++ ADS3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    float radius = 1.1;
+    float area;
+    
+    area = (3.14 * radius * radius);
+    
+    printf("\n Area of Circle = %f\n", area);
+    
+    return 0;
+}
