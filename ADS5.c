1573a39c5eaab97acde883abb2ee2bfd6d23225f
Adityaraj Salunke <salunkeaditya0095@gmail.com>
2026-09-04 21:49:50 +0530

    Add files via upload

---
+++ ADS5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() 
+{
+    int a = 33;
+    int b = 47;
+    int c = 20;
+    float average;
+ 
+    average = (a + b + c) / 3.0; 
+    printf("\n Average = %.2f\n", average);
+    
+    return 0;
+}
